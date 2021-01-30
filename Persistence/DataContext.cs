d5a36d3aee06a3782f843396023619ede524f329
Kerminator1973 <maxim.l.rozhkov@gmail.com>
2021-01-30 23:03:18 +0300

    first commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain;                           // Определение структуры (объекта) Value
+using Microsoft.EntityFrameworkCore;    // Определение DbContext
+
+namespace Persistence
+{
+    // DbContext представляет интерфейс для доступа к базе данных
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // Указываем на необходимость включения в базу данных таблицы Values
+        public DbSet<Value> Values { get; set; }
+
+        // Добавляем Seed - стартовые данные, без которых нормальная работа
+        // приложения может быть затруднена. Обычно Seed содержат редко
+        // изменяемые справочники
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value {Id = 1, Name = "Value 101"},
+                    new Value {Id = 2, Name = "Value 102"},
+                    new Value {Id = 3, Name = "Value 103"}
+                );
+        }
+    }
+}
