9196f42f311c8fc808f3f8bca306a1e6a4284b8e
Kerminator1973 <maxim.l.rozhkov@gmail.com>
2021-02-19 21:23:40 +0300

    Перевод React-кода на Hooks

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,35 +1,33 @@
-import React, {Component} from 'react';
+import React, { useEffect, useState} from 'react';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from './models/activity';
 
-class App extends Component {
+function App()  {
 
-  state = {
-    values: []
-  }
+  // Определяем состояние функционального компонента. Используя 
+  // специализацию, явно указываем тип состояния, что обеспечивает
+  // контроль типов при их использовании, см. activities.map()
+  const [activities, setActivities] = useState<Activity[]>([]);
 
-  componentDidMount() {
-    axios.get('http://localhost:5000/api/activities')
-      .then(response =>{
-        this.setState({
-          values: response.data
-        })
+  // Загружаем список элементов из API, используя Axios
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
+      .then(response => {
+          setActivities(response.data);
       })
-  }
+  }, [])
 
-  render() {
-
-    return (
-      <div>
-        <Header as='h2' icon='users' content='Reactivities' />
-          <List>
-            {this.state.values.map((value: any) => (
-              <List.Item key={value.id}>{value.title}</List.Item>
-            ))}
-          </List>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' />
+        <List>
+          {activities.map((activity) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+    </div>
+  );
 }
 
 export default App;
