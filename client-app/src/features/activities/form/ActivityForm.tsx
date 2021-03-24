6d57d1a47a79f554463a6f5ccb0361a610b1cefa
Kerminator1973 <maxim.l.rozhkov@gmail.com>
2021-03-24 21:09:25 +0300

    Добавлен custom input field

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,12 +1,14 @@
 import { observer } from 'mobx-react-lite';
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
-import { Button, FormField, Label, Segment } from 'semantic-ui-react';
+import { Button, Label, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
 import {v4 as uuid} from 'uuid';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import MyTextInput from '../../../app/common/form/MyTextInput';
+import MyTextArea from './MyTextArea';
 
 export default observer (function ActivityForm() {
 
@@ -30,8 +32,14 @@ export default observer (function ActivityForm() {
         venue: ''
     });
 
+    // Определяем схему валидации значений, вводимых в полях формы
     const validationSchema = Yup.object({
-        title: Yup.string().required('The activity title is required')
+        title: Yup.string().required('The activity title is required'),
+        description: Yup.string().required('The activity description is required'),
+        category: Yup.string().required('The category title is required'),
+        date: Yup.string().required('The activity date is required'),
+        venue: Yup.string().required('The activity venue is required'),
+        city: Yup.string().required('The activity city is required')
     });
 
     // В чтсле параметров хука используется список состоящий из переменных
@@ -85,17 +93,12 @@ export default observer (function ActivityForm() {
                 onSubmit={values => console.log(values)}>
                 {({handleSubmit}) => (
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                        <FormField>
-                            <Field placeholder="Title" name='title' />
-                            <ErrorMessage name='title' 
-                                render={error => <Label basic color='red' content={error} />} />
-                        </FormField>
-                        
-                        <Field placeholder="Description" name='description' />
-                        <Field placeholder="Category" name='category' />
-                        <Field type='date' placeholder="Date" name='date' />
-                        <Field placeholder="City" name='city' />
-                        <Field placeholder="Venue" name='venue' />
+                        <MyTextInput name='title' placeholder='Title' />
+                        <MyTextArea rows={3} placeholder="Description" name='description' />
+                        <MyTextInput placeholder="Category" name='category' />
+                        <MyTextInput placeholder="Date" name='date' />
+                        <MyTextInput placeholder="City" name='city' />
+                        <MyTextInput placeholder="Venue" name='venue' />
                         <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                         <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
                     </Form>
