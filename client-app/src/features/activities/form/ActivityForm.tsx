5d7b846f36d7fa4ae5d8a0f59ec4570be6050d70
Kerminator1973 <maxim.l.rozhkov@gmail.com>
2021-03-27 12:36:10 +0300

    Восстановлена работоспособность кнопки Submit

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,11 +1,11 @@
 import { observer } from 'mobx-react-lite';
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory, useParams } from 'react-router-dom';
-import { Button, Label, Segment } from 'semantic-ui-react';
+import { Button, Header, Segment } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
 import {v4 as uuid} from 'uuid';
-import { Formik, Form, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import MyTextInput from '../../../app/common/form/MyTextInput';
 import MyTextArea from '../../../app/common/form/MyTextArea';
@@ -41,7 +41,7 @@ export default observer (function ActivityForm() {
         title: Yup.string().required('The activity title is required'),
         description: Yup.string().required('The activity description is required'),
         category: Yup.string().required('The category title is required'),
-        date: Yup.string().required('The activity date is required'),
+        date: Yup.string().required('The activity date is required').nullable(),
         venue: Yup.string().required('The activity venue is required'),
         city: Yup.string().required('The activity city is required')
     });
@@ -54,10 +54,9 @@ export default observer (function ActivityForm() {
         if (id) loadActivity(id).then(activity => setActivity(activity!))
     }, [id, loadActivity]);
 
-/*    
     // В обработчике нажатия на кнопку "Submit", в зависимости от установленного режима,
     // по разному устанавливаем адрес возврата к предыдущей форме
-    function handleSubmit() {
+    function handleFromSubmit(activity: Activity) {
 
         if (activity.id.length === 0) {
             
@@ -77,25 +76,17 @@ export default observer (function ActivityForm() {
         }
     }
 
-    // Функция обрабатываем событие об изменении содержимого Input и TextArea.
-    // При получении события в словарь полей (activity) изменяется значение поля
-    // "name" на указанное в "value". Изменение осуществляется посредством spread-оператор
-    function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-        const {name, value} = event.target;
-        setActivity({...activity, [name]: value});
-    }
-*/    
-
     if (loadingInitial) return <LoadingComponent content='Loading activity...' />
 
     return (
         <Segment clearing>
+            <Header content='Activity Details' sub color='teal' />
             <Formik 
                 validationSchema={validationSchema}
                 enableReinitialize 
                 initialValues={activity} 
-                onSubmit={values => console.log(values)}>
-                {({handleSubmit}) => (
+                onSubmit={values => handleFromSubmit(values)}>
+                {({handleSubmit, isValid, isSubmitting, dirty}) => (
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
                         <MyTextInput name='title' placeholder='Title' />
                         <MyTextArea rows={3} placeholder="Description" name='description' />
@@ -107,9 +98,12 @@ export default observer (function ActivityForm() {
                             timeCaption='time'
                             dateFormat='MMMM d, yyyy h:mm aa'
                         />
+                        <Header content='Location Details' sub color='teal' />
                         <MyTextInput placeholder="City" name='city' />
                         <MyTextInput placeholder="Venue" name='venue' />
-                        <Button loading={loading} floated='right' positive type='submit' content='Submit' />
+                        <Button 
+                            disabled={isSubmitting || !dirty || !isValid}
+                            loading={loading} floated='right' positive type='submit' content='Submit' />
                         <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
                     </Form>
                 )}
