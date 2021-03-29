83870a5eef7709af34a651167b94e0bf9764f912
Kerminator1973 <maxim.l.rozhkov@gmail.com>
2021-03-29 20:00:28 +0300

    Сервисы Identity настроены

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,28 @@
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, 
+            IConfiguration config) 
+        {
+            // Определяем стратегию паролей. В конкретном случае указываем,
+            // что можно обойтись алфавитно-цифровым паролем, без спец.символов
+            services.AddIdentityCore<AppUser>(opt => 
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+            .AddEntityFrameworkStores<DataContext>()
+            .AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddAuthentication();
+
+            return services;
+        }
+    }
+}
