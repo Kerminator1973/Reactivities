ee04fa1cffe8209a476065fcccab05da6d96294c
Kerminator1973 <maxim.l.rozhkov@gmail.com>
2021-04-04 22:34:23 +0300

    Полученный токен сохраняется в localStorage

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,6 +1,8 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { User, UserFormValues } from "../models/user";
+import { store } from "./store";
+import { history } from '../..';    // См.: createBrowserHistory()
 
 export default class UserStore {
     user!: User | null;
@@ -16,11 +18,27 @@ export default class UserStore {
     login = async (creds: UserFormValues) => {
         try {
             const user = await agent.Account.login(creds);
-            console.log(user);
+
+            // Сохраняем токен в Store (localStorage)
+            store.commonStore.setToken(user.token);
+
+            // Поскольку user устанавливается как результат await-операции,
+            // необходимо использовать runInAction()
+            runInAction(() => this.user = user);
+
+            // Переходим на главную страницу приложения
+            history.push('/activities');
         }
         catch (error)
         {
             throw error;   
         }
+    }
+
+    logout = () => {
+        store.commonStore.setToken(null);
+        window.localStorage.removeItem('jwt');
+        this.user = null;
+        history.push('/');
     }
 }
