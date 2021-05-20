737776eaa244a4f5892da203945ea5f04bb11969
Maksim Rozhkov <kermit@mail.ru>
2021-05-20 19:50:14 +0300

    Добавлены сущности Followers/Followinfs

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -21,6 +21,7 @@ namespace Persistence
 
         public DbSet<Comment> Comments { get; set; }
 
+        public DbSet<UserFollowing> UserFollowings { get; set; }
 
         // Добавляем Seed - стартовые данные, без которых нормальная работа
         // приложения может быть затруднена. Обычно Seed содержат редко
@@ -62,6 +63,20 @@ namespace Persistence
                 .HasOne(a => a.Activity)
                 .WithMany(c => c.Comments)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserFollowing>(b => {
+                b.HasKey(k => new {k.ObserverId, k.TargetId});
+
+                b.HasOne(o => o.Observer)
+                    .WithMany(f => f.Followings)
+                    .HasForeignKey(o => o.ObserverId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                b.HasOne(o => o.Target)
+                    .WithMany(f => f.Followers)
+                    .HasForeignKey(o => o.TargetId)
+                    .OnDelete(DeleteBehavior.Cascade);                    
+            });
         }
     }
 }
